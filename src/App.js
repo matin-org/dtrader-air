8886f2db406b9cbc730b3044fcf2037fc404855b
Prince <prince@deriv.com>
2022-10-21 16:58:49 +0800

    chore: initial files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import Cookies from "js-cookie";
+import { useState } from "react";
 import "./App.css";
 import { APP_ID } from "./helpers";
 
 function App() {
+  const [token, setToken] = useState(Cookies.get("lt_token"));
+
   const query_string = window.location.search;
 
   const UrlParams = new URLSearchParams(query_string);
 
-  const token = UrlParams.get("token1");
+  const auth_token = UrlParams.get("token1");
 
-  Cookies.set("lt_token", token);
+  if (auth_token) {
+    Cookies.set("lt_token", auth_token);
+  }
+
+  if (!token || token === "null") {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            const login_url = `https://oauth.deriv.com/oauth2/authorize?app_id=${APP_ID}&l=en&brand=light-trader`;
+
+            window.location = login_url;
+          }}
+        >
+          Log In
+        </button>
+      </div>
+    );
+  }
 
   return (
     <div className="App">
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,4 @@
-import { APP_ID } from "./contants";
+import { APP_ID, TOKEN_KEY, DEFAULT_LOCAL_URL } from "./contants";
+import { isBrowser, isLocal } from "./utilities";
 
-export { APP_ID };
+export { APP_ID, TOKEN_KEY, DEFAULT_LOCAL_URL, isBrowser, isLocal };
