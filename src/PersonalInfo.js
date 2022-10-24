9ec750f1711299540adda4269af1b7b7e41edff5
habib-deriv <habib@regentmarkets.com>
2022-10-24 16:22:24 +0600

    add fixes

---
+++ src/PersonalInfo.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react'
+
+export default function PersonalInfo() {
+    const [balence, setBalence] = useState('');
+    const [email, setEmail] = useState('');
+    
+    return (
+        <h1>PersonalInfo</h1>
+    )
+}
