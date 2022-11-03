2f5fdcb03afdfd7f953e00fbf8e04d5192cfcbac
prince-deriv <82309725+prince-deriv@users.noreply.github.com>
2022-11-03 08:53:03 +0400

    Prince/ Trade components 2 (#10)
    * chore: added account list and trade types fetching
    
    * chore: added some trading components
    
    * chore: conflicts fixed

--- src/components/layout/trade.js
+++ src/components/layout/trade.js
@@ -4,7 +4,6 @@ import { useCheckLogin } from "components/hooks";
 import { useDtraderAirWS, DtraderAirStore } from "store";
 import { TradeTypes, Duration, Stake } from "components/widgets";
 
-
 const Trade = () => {
   const { is_logged_in, token } = useCheckLogin();
   const [is_loading, setLoading] = useState(true);
@@ -14,8 +13,6 @@ const Trade = () => {
     currency: "",
     email: "",
   });
-
-  const [contracts, setContracts] = useState([]);
 
   const { send } = useDtraderAirWS();
   const { useAccounts } = React.useContext(DtraderAirStore);
@@ -42,30 +39,6 @@ const Trade = () => {
       setLoading(false);
     }
   }, []);
-
-  // Fetch Contracts for symbol
-  useEffect(() => {
-    if (symbol) {
-      send(
-        {
-          contracts_for: symbol,
-          product_type: "basic",
-        },
-        (response) => {
-          if (response.contracts_for) {
-            const { contracts_for } = response;
-            const { available } = contracts_for;
-
-            const filtered_contracts = [
-              ...new Set(available.map((c) => c.contract_category_display)),
-            ];
-
-            setContracts(filtered_contracts);
-          }
-        }
-      );
-    }
-  }, [symbol]);
 
   if (is_loading) {
     return <></>;
